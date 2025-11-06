f8a69ae4da12969bd78612df1e7ecec59d00afe7
Lea <leawork25@gmail.com>
2025-11-05 21:13:47 -0700

    finished feed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,23 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Feed.css'
-import thumbnail1 from '../../assets/thumbnail1.png'
-import thumbnail2 from '../../assets/thumbnail2.png'
-import thumbnail3 from '../../assets/thumbnail3.png'
-import thumbnail4 from '../../assets/thumbnail4.png'
-import thumbnail5 from '../../assets/thumbnail5.png'
-import thumbnail6 from '../../assets/thumbnail6.png'
-import thumbnail7 from '../../assets/thumbnail7.png'
-import thumbnail8 from '../../assets/thumbnail8.png'
-import { API_KEY } from '../../Data'
+import { API_KEY, value_converter } from '../../Data'
+import moment from 'moment'
 
 
 const Feed = ({category}) => {
-    const [data, setData] = useState({});
+    const [data, setData] = useState([]);
 
     const fetchData = async () => {
         const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
-        await fetch(videoList_url).then(response=>response.json()).then(data=>setData(data=>setData(data.items)))
+        await fetch(videoList_url).then(response=>response.json()).then(data=>setData(data.items))
     }
 
     useEffect (() => {
@@ -29,11 +22,11 @@ const Feed = ({category}) => {
     <div className="feed">
         {data.map((item, index) => {
             return (
-                <Link to={`video/${item.snippet.categoryID}/${item.id}`} className='card'>
-                    <img src={thumbnail1} alt="" />
-                    <h2>Best chanel to learn coding that help you to be a web developer.</h2>
-                    <h3>Greatstack</h3>
-                    <p>15k views &bull; 2 days ago</p>
+                <Link to={`video/${item.snippet.categoryId}/${item.id}`} className='card'>
+                    <img src={item.snippet.thumbnails.medium.url} alt="" />
+                    <h2>{item.snippet.title}</h2>
+                    <h3>{item.snippet.channelTitle}</h3>
+                    <p>{value_converter(item.statistics.viewCount)} &bull; {moment(item.snippet.publishedAt).fromNow()}</p>
                 </Link>
             )
         })}
--- src/Data.js
+++ src/Data.js
@@ -1,1 +1,13 @@
 export const API_KEY = "AIzaSyBM3k3Ip7D6ETl9G8rlT7n0b_YEJeQzLdg";
+
+export const value_converter = (value) => {
+    if(value >= 1000000) {
+        return Math.floor(value/1000000)+"M";
+    }
+    else if (value>=1000) {
+        return Math.floor(value/1000)+"K"
+    }
+    else {
+        return value
+    }
+}
